e5401f75d08246ad9c11fea5ff72cc3850286e4c
PhamHoangTuanTu <114724635+PhamHoangTuanTu@users.noreply.github.com>
2022-09-30 14:25:05 +0700

    Add files via upload

---
+++ PhamHoangTuanTu_22102371_Assignment01/PhamHoangTuanTu_22102371_Assignment01/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace PhamHoangTuanTu_22102371_Assignment01
+{
+    partial class c
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_top = new System.Windows.Forms.Panel();
+            this.button_dashboard = new System.Windows.Forms.Button();
+            this.button_settings = new System.Windows.Forms.Button();
+            this.button_help = new System.Windows.Forms.Button();
+            this.panel_green = new System.Windows.Forms.Panel();
+            this.button_green_on = new System.Windows.Forms.Button();
+            this.button_green_off = new System.Windows.Forms.Button();
+            this.panel_red = new System.Windows.Forms.Panel();
+            this.button_red_on = new System.Windows.Forms.Button();
+            this.button_red_off = new System.Windows.Forms.Button();
+            this.panel_yellow = new System.Windows.Forms.Panel();
+            this.button_yellow_on = new System.Windows.Forms.Button();
+            this.button_yellow_off = new System.Windows.Forms.Button();
+            this.panel_fill = new System.Windows.Forms.Panel();
+            this.label_time_red = new System.Windows.Forms.Label();
+            this.label_time_yellow = new System.Windows.Forms.Label();
+            this.label_time_green = new System.Windows.Forms.Label();
+            this.panel_top.SuspendLayout();
+            this.panel_green.SuspendLayout();
+            this.panel_red.SuspendLayout();
+            this.panel_yellow.SuspendLayout();
+            this.panel_fill.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_top
+            // 
+            this.panel_top.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel_top.Controls.Add(this.button_help);
+            this.panel_top.Controls.Add(this.button_settings);
+            this.panel_top.Controls.Add(this.button_dashboard);
+            this.panel_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_top.Location = new System.Drawing.Point(0, 0);
+            this.panel_top.Name = "panel_top";
+            this.panel_top.Size = new System.Drawing.Size(800, 122);
+            this.panel_top.TabIndex = 0;
+            // 
+            // button_dashboard
+            // 
+            this.button_dashboard.Location = new System.Drawing.Point(81, 29);
+            this.button_dashboard.Name = "button_dashboard";
+            this.button_dashboard.Size = new System.Drawing.Size(149, 60);
+            this.button_dashboard.TabIndex = 0;
+            this.button_dashboard.Text = "Dashboard";
+            this.button_dashboard.UseVisualStyleBackColor = true;
+            // 
+            // button_settings
+            // 
+            this.button_settings.Location = new System.Drawing.Point(313, 29);
+            this.button_settings.Name = "button_settings";
+            this.button_settings.Size = new System.Drawing.Size(169, 59);
+            this.button_settings.TabIndex = 1;
+            this.button_settings.Text = "Settings";
+            this.button_settings.UseVisualStyleBackColor = true;
+            // 
+            // button_help
+            // 
+            this.button_help.Location = new System.Drawing.Point(569, 29);
+            this.button_help.Name = "button_help";
+            this.button_help.Size = new System.Drawing.Size(152, 60);
+            this.button_help.TabIndex = 2;
+            this.button_help.Text = "Help";
+            this.button_help.UseVisualStyleBackColor = true;
+            // 
+            // panel_green
+            // 
+            this.panel_green.BackColor = System.Drawing.Color.Green;
+            this.panel_green.Controls.Add(this.button_green_on);
+            this.panel_green.Controls.Add(this.button_green_off);
+            this.panel_green.Location = new System.Drawing.Point(542, 63);
+            this.panel_green.Name = "panel_green";
+            this.panel_green.Size = new System.Drawing.Size(231, 119);
+            this.panel_green.TabIndex = 5;
+            // 
+            // button_green_on
+            // 
+            this.button_green_on.Location = new System.Drawing.Point(43, 18);
+            this.button_green_on.Name = "button_green_on";
+            this.button_green_on.Size = new System.Drawing.Size(55, 50);
+            this.button_green_on.TabIndex = 2;
+            this.button_green_on.Text = "On";
+            this.button_green_on.UseVisualStyleBackColor = true;
+            // 
+            // button_green_off
+            // 
+            this.button_green_off.BackColor = System.Drawing.Color.Gray;
+            this.button_green_off.Location = new System.Drawing.Point(131, 18);
+            this.button_green_off.Name = "button_green_off";
+            this.button_green_off.Size = new System.Drawing.Size(55, 50);
+            this.button_green_off.TabIndex = 1;
+            this.button_green_off.Text = "Off";
+            this.button_green_off.UseVisualStyleBackColor = false;
+            // 
+            // panel_red
+            // 
+            this.panel_red.BackColor = System.Drawing.Color.Red;
+            this.panel_red.Controls.Add(this.button_red_on);
+            this.panel_red.Controls.Add(this.button_red_off);
+            this.panel_red.Location = new System.Drawing.Point(12, 63);
+            this.panel_red.Name = "panel_red";
+            this.panel_red.Size = new System.Drawing.Size(235, 119);
+            this.panel_red.TabIndex = 6;
+            // 
+            // button_red_on
+            // 
+            this.button_red_on.Location = new System.Drawing.Point(43, 18);
+            this.button_red_on.Name = "button_red_on";
+            this.button_red_on.Size = new System.Drawing.Size(55, 50);
+            this.button_red_on.TabIndex = 2;
+            this.button_red_on.Text = "On";
+            this.button_red_on.UseVisualStyleBackColor = true;
+            // 
+            // button_red_off
+            // 
+            this.button_red_off.BackColor = System.Drawing.Color.DimGray;
+            this.button_red_off.Location = new System.Drawing.Point(147, 18);
+            this.button_red_off.Name = "button_red_off";
+            this.button_red_off.Size = new System.Drawing.Size(55, 50);
+            this.button_red_off.TabIndex = 1;
+            this.button_red_off.Text = "Off";
+            this.button_red_off.UseVisualStyleBackColor = false;
+            // 
+            // panel_yellow
+            // 
+            this.panel_yellow.BackColor = System.Drawing.Color.Yellow;
+            this.panel_yellow.Controls.Add(this.button_yellow_on);
+            this.panel_yellow.Controls.Add(this.button_yellow_off);
+            this.panel_yellow.Location = new System.Drawing.Point(281, 63);
+            this.panel_yellow.Name = "panel_yellow";
+            this.panel_yellow.Size = new System.Drawing.Size(240, 119);
+            this.panel_yellow.TabIndex = 7;
+            // 
+            // button_yellow_on
+            // 
+            this.button_yellow_on.Location = new System.Drawing.Point(43, 18);
+            this.button_yellow_on.Name = "button_yellow_on";
+            this.button_yellow_on.Size = new System.Drawing.Size(55, 50);
+            this.button_yellow_on.TabIndex = 2;
+            this.button_yellow_on.Text = "On";
+            this.button_yellow_on.UseVisualStyleBackColor = true;
+            // 
+            // button_yellow_off
+            // 
+            this.button_yellow_off.BackColor = System.Drawing.Color.Gray;
+            this.button_yellow_off.Location = new System.Drawing.Point(149, 18);
+            this.button_yellow_off.Name = "button_yellow_off";
+            this.button_yellow_off.Size = new System.Drawing.Size(55, 50);
+            this.button_yellow_off.TabIndex = 1;
+            this.button_yellow_off.Text = "Off";
+            this.button_yellow_off.UseVisualStyleBackColor = false;
+            // 
+            // panel_fill
+            // 
+            this.panel_fill.BackColor = System.Drawing.Color.Bisque;
+            this.panel_fill.Controls.Add(this.label_time_green);
+            this.panel_fill.Controls.Add(this.label_time_yellow);
+            this.panel_fill.Controls.Add(this.label_time_red);
+            this.panel_fill.Controls.Add(this.panel_green);
+            this.panel_fill.Controls.Add(this.panel_red);
+            this.panel_fill.Controls.Add(this.panel_yellow);
+            this.panel_fill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_fill.Location = new System.Drawing.Point(0, 122);
+            this.panel_fill.Name = "panel_fill";
+            this.panel_fill.Size = new System.Drawing.Size(800, 328);
+            this.panel_fill.TabIndex = 1;
+            // 
+            // label_time_red
+            // 
+            this.label_time_red.AutoSize = true;
+            this.label_time_red.BackColor = System.Drawing.Color.Red;
+            this.label_time_red.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_time_red.Location = new System.Drawing.Point(105, 201);
+            this.label_time_red.Name = "label_time_red";
+            this.label_time_red.Size = new System.Drawing.Size(59, 25);
+            this.label_time_red.TabIndex = 8;
+            this.label_time_red.Text = "Time";
+            this.label_time_red.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_time_yellow
+            // 
+            this.label_time_yellow.AutoSize = true;
+            this.label_time_yellow.BackColor = System.Drawing.Color.Yellow;
+            this.label_time_yellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_time_yellow.Location = new System.Drawing.Point(372, 201);
+            this.label_time_yellow.Name = "label_time_yellow";
+            this.label_time_yellow.Size = new System.Drawing.Size(59, 25);
+            this.label_time_yellow.TabIndex = 9;
+            this.label_time_yellow.Text = "Time";
+            // 
+            // label_time_green
+            // 
+            this.label_time_green.AutoSize = true;
+            this.label_time_green.BackColor = System.Drawing.Color.Green;
+            this.label_time_green.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_time_green.Location = new System.Drawing.Point(627, 201);
+            this.label_time_green.Name = "label_time_green";
+            this.label_time_green.Size = new System.Drawing.Size(59, 25);
+            this.label_time_green.TabIndex = 10;
+            this.label_time_green.Text = "Time";
+            // 
+            // c
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_fill);
+            this.Controls.Add(this.panel_top);
+            this.Name = "c";
+            this.Text = "Form1";
+            this.panel_top.ResumeLayout(false);
+            this.panel_green.ResumeLayout(false);
+            this.panel_red.ResumeLayout(false);
+            this.panel_yellow.ResumeLayout(false);
+            this.panel_fill.ResumeLayout(false);
+            this.panel_fill.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_top;
+        private System.Windows.Forms.Button button_help;
+        private System.Windows.Forms.Button button_settings;
+        private System.Windows.Forms.Button button_dashboard;
+        private System.Windows.Forms.Panel panel_green;
+        private System.Windows.Forms.Button button_green_on;
+        private System.Windows.Forms.Button button_green_off;
+        private System.Windows.Forms.Panel panel_red;
+        private System.Windows.Forms.Button button_red_on;
+        private System.Windows.Forms.Button button_red_off;
+        private System.Windows.Forms.Panel panel_yellow;
+        private System.Windows.Forms.Button button_yellow_on;
+        private System.Windows.Forms.Button button_yellow_off;
+        private System.Windows.Forms.Panel panel_fill;
+        private System.Windows.Forms.Label label_time_red;
+        private System.Windows.Forms.Label label_time_green;
+        private System.Windows.Forms.Label label_time_yellow;
+    }
+}
+
